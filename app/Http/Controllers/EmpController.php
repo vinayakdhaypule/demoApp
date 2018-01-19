22c48c6f561b321a2b7f5fff98ba5009d74c4ebe
vinayakdhaypule <vinayakdhaypule@gmail.com>
2018-01-19 17:54:20 +0530

    Add new emp controller

---
+++ app/Http/Controllers/EmpController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Redirct;
+use Auth;
+use Session;
+
+class EmpController extends Controller {
+    
+    public function getSignupPage() {
+    
+}
